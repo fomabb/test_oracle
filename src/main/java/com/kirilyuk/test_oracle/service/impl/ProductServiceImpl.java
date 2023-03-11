bf7ccf8aa719075900ef084542de435597012e47
Nikolay Kirilyk <fomabb@yandex.by>
2023-03-11 14:35:43 +0300

    create new methods in test2

--- src/main/java/com/kirilyuk/test_oracle/service/impl/ProductServiceImpl.java
+++ src/main/java/com/kirilyuk/test_oracle/service/impl/ProductServiceImpl.java
@@ -11,6 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
