7debf5f8b1e9cc0237a083a713c76c4b5a971f49
fomabb <fomabb@yandex.by>
2023-03-18 19:44:38 +0300

    create date time format

--- src/main/java/com/kirilyuk/test_oracle/TestOracleApplication.java
+++ src/main/java/com/kirilyuk/test_oracle/TestOracleApplication.java
@@ -2,8 +2,10 @@ package com.kirilyuk.test_oracle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:database.properties")
 public class TestOracleApplication {
 
     public static void main(String[] args) {
